5df5c75f4ae40c890a983ad50110c33258198e76
delonce <pavel.piksel2012@mail.ru>
2024-12-13 00:32:11 +0300

    feat: Demo servlet for Tomcat

--- src/main/java/org/delonce/DelonceServlet.java
+++ src/main/java/org/delonce/DelonceServlet.java
@@ -8,7 +8,6 @@ import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 
-@WebServlet(name = "DelonceServlet", urlPatterns = {"test"}, loadOnStartup = 1)
 public class DelonceServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
