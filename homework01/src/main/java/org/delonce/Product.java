616c30bbb592405ff14fcb6223e2cff6a913c325
delonce <pavel.piksel2012@mail.ru>
2024-12-16 00:14:52 +0300

    style: Remove empty spaces

--- homework01/src/main/java/org/delonce/Product.java
+++ homework01/src/main/java/org/delonce/Product.java
@@ -1,12 +1,10 @@
 package org.delonce;
 
-
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
-
 
 @Getter
 @Setter
