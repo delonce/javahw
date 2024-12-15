18493c3f27a30104a5ef583c41c913f4a1b0108b
delonce <pavel.piksel2012@mail.ru>
2024-12-15 23:59:01 +0300

    style: Each project as maven module

---
+++ homework01/src/main/java/org/delonce/Product.java
@@ -0,0 +1,21 @@
+package org.delonce;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Product {
+    private int id;
+    private String description;
+    private double price;
+    private int amount;
+}
