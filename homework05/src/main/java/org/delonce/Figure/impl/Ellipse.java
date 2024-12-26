af95f20438c523e1bfbdee45c99b18d44ea4bb7d
delonce <pavel.piksel2012@mail.ru>
2024-12-26 18:58:14 +0300

    feat: HW5

---
+++ homework05/src/main/java/org/delonce/Figure/impl/Ellipse.java
@@ -0,0 +1,25 @@
+package org.delonce.Figure.impl;
+
+public class Ellipse extends Circle {
+    private final double D;
+
+    public Ellipse(int X, int Y, double d, double D) {
+        super(X, Y, d);
+        this.D = D;
+    }
+
+    @Override
+    public double getPerimeter() {
+        return 2 * Math.PI *
+                Math.sqrt(
+                        (Math.pow(getRadius(), 2) + Math.pow(this.D, 2)) / 2
+                );
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + "Ellipse{" +
+                "D=" + D +
+                '}';
+    }
+}
