4160b8f689098de007fbddcfe0096313d55afd82
delonce <pavel.piksel2012@mail.ru>
2025-01-09 14:49:27 +0300

    feat: hw08

---
+++ homework08/src/main/java/org/delonce/Main.java
@@ -0,0 +1,28 @@
+package org.delonce;
+
+import java.util.Objects;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+public class Main {
+    public static void main(String[] args) {
+        Predicate<Object> condition= Objects::isNull;
+        Function<Object,Integer>ifTrue=obj->0;
+        Function<CharSequence,Integer> ifFalse=CharSequence::length;
+
+        Function<String,Integer> safeStringLength = ternaryOperator(condition, ifTrue, ifFalse);
+
+        System.out.println(safeStringLength.apply("hello"));
+        System.out.println(safeStringLength.apply(null));
+    }
+
+    private static <T, R> Function<T, R> ternaryOperator(Predicate<Object> condition, Function<? super T, R> ifTrue, Function<? super T, R> ifFalse) {
+        return (s) -> {
+            if (condition.test(s)) {
+                return ifTrue.apply(s);
+            } else {
+                return ifFalse.apply(s);
+            }
+        };
+    }
+}
